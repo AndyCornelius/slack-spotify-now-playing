04a9e5737cdd31f8578ae567010f76ddf59dcffa
AndyCornelius <Andrew_Cornelius@hotmail.co.uk>
2018-06-01 13:27:09 +0100

    original bash script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# slack-spotify-now-playing
+# Slack/Spotify Now Playing
+
 Bash script for setting your Slack status based on the current song on Spotify
+
+Requires curl, PHP and osascript for command line AppleScript.
+
+I'm running this on a every minute cron task, so it runs approx. every 30 seconds.
---
+++ slackNowPlaying.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+APIKEY="your api key here"
+trap onexit INT
+
+function getExistingStatus() {
+    if [ ! -f status_emoji.txt ] && [ ! -f status_text.txt ]; then
+        CURRENTPROFILE=$(curl -s "https://slack.com/api/users.profile.get?token="$APIKEY)
+        STATUSEMOJI=$(php -r '$result = json_decode($argv[1], true); echo $result["profile"][$argv[2]];' "$CURRENTPROFILE" status_emoji)
+
+        if [[ $STATUSEMOJI != "" && $STATUSEMOJI != ":headphones:" ]]; then
+            echo $STATUSEMOJI > status_emoji.txt
+
+            echo $(php -r '$result = json_decode($argv[1], true); echo $result["profile"][$argv[2]];' "$CURRENTPROFILE" status_text) > status_text.txt
+        fi
+    fi
+}
+
+function updateSlackStatus() {
+    URLEMOJI=$(php -r 'echo rawurlencode($argv[1]);' "$2")
+    URLTEXT=$(php -r 'echo rawurlencode($argv[1]);' "$1")
+
+    curl -s -d "payload=$json" "https://slack.com/api/users.profile.set?token="$APIKEY"&profile=%7B%22status_text%22%3A%22"$URLTEXT"%22%2C%22status_emoji%22%3A%22"$URLEMOJI"%22%7D" > /dev/null
+}
+
+function reset() {
+    if [ -f status_emoji.txt ]; then
+        EMOJI=$(cat status_emoji.txt)
+    else
+        EMOJI=""
+    fi
+
+    if [ -f status_text.txt ]; then
+        TEXT=$(cat status_text.txt)
+    else
+        TEXT=""
+    fi
+
+    updateSlackStatus "$TEXT" "$EMOJI"
+}
+
+function onexit() {
+    reset
+
+    rm -f status_*.txt
+}
+
+function runMainLogic() {
+    if [[ $(osascript -e 'tell application "System Events" to get name of every process') = *"Slack"* ]]; then
+        getExistingStatus
+
+        if [[ $(osascript -e 'tell application "System Events" to get name of every process') = *"Spotify"* ]]; then
+            STATE=$(osascript -e 'tell application "Spotify" to player state')
+
+            if [[ "$STATE" != "playing" ]]; then
+                reset
+            else
+                SONG=$(osascript -e 'tell application "Spotify" to artist of current track & " - " & name of current track')
+
+                updateSlackStatus "$SONG" ":headphones:"
+            fi
+        else
+            reset
+        fi
+    else
+        rm -f status_*.txt
+    fi
+}
+
+runMainLogic
+
+sleep 30
+
+runMainLogic
